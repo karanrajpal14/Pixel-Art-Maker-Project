20987b0edf2e9da03b53cf987af86dda07accfcd
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-03-21 12:15:25 +0530

    Prepared base structure

--- designs.js
+++ designs.js
@@ -1,10 +1,14 @@
 // Select color input
+var colorInput = $("#colorPicker");
+console.log(colorInput.val());
+
 // Select size input
+var height = $("#inputHeight");
+var width = $("#inputWeight");
+console.log(height.val(), width.val());
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+  console.log("Making a " + width.val() + "x" + height.val() + " grid.");
 }
