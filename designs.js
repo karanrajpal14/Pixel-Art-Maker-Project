d5c4329b12a92c33e1006e19b50af9206001ad95
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-03-22 10:16:55 +0530

    Reformatted code adhering to Udacity's style guide
    ## Refer here for more information
    
    - HTML: [1]
    - CSS: [2]
    - JavaScript: [3]
    
    [1]: http://udacity.github.io/frontend-nanodegree-styleguide/index.html
    [2]: http://udacity.github.io/frontend-nanodegree-styleguide/css.html
    [3]: http://udacity.github.io/frontend-nanodegree-styleguide/javascript.html

--- designs.js
+++ designs.js
@@ -1,42 +1,42 @@
-var pixelCanvas = $("#pixelCanvas");
+const PIXEL_GRID = $('#pixelCanvas');
 
 // Event Listener to get the clicked cell and color it
-pixelCanvas.on("click", "td", function() {
+PIXEL_GRID.on('click', 'td', function() {
   // Select color input value
-  var pickedColor = $("#colorPicker").val();
+  const PICKED_COLOR = $('#colorPicker').val();
 
-  // Get clicked element and their respective ids
-  var clickedElement = $(this)[0];
-  var columnID = "#" + clickedElement.id;
-  var rowID = "#" + clickedElement.parentElement.id;
-  var element = $(rowID).find(columnID);
+  // Get ID of clicked cell
+  const CLICKED_ELEMENT = $(this)[0];
+  const COLUMN_ID = '#' + CLICKED_ELEMENT.id;
+  const ROW_ID = '#' + CLICKED_ELEMENT.parentElement.id;
+  const CELL = $(ROW_ID).find(COLUMN_ID);
 
   // Color clicked cell
-  element.css("background-color", pickedColor);
+  CELL.css('background-color', PICKED_COLOR);
 });
 
 // Function to create the cell grid
 function makeGrid() {
   // Fetching rows and columns from input
-  var totalColumns = $("#inputHeight").val();
-  var totalRows = $("#inputWeight").val();
+  const TOTAL_COLUMNS = $('#inputHeight').val();
+  const TOTAL_ROWS = $('#inputWeight').val();
 
   // Clearing the canvas
-  pixelCanvas.empty();
+  PIXEL_GRID.empty();
 
   // Generating grid rows and appending to table in DOM
-  for (var i = 1; i <= totalRows; i++) {
-    var rowId = "row" + i;
-    var rowIDAttr = "id='" + rowId + "'";
-    var tr = $("<tr " + rowIDAttr + "></tr>");
-    pixelCanvas.append(tr);
+  for (let i = 1; i <= TOTAL_ROWS; i++) {
+    let rowId = 'row' + i;
+    let rowIDAttr = 'id="' + rowId + '"';
+    let tableRow = $('<tr ' + rowIDAttr + '></tr>');
+    PIXEL_GRID.append(tableRow);
 
     // Generating columns and appending to row in DOM
-    for (var j = 1; j <= totalColumns; j++) {
-      var colID = "col" + j;
-      var colIDAttr = "id='" + colID + "'";
-      var td = $("<td " + colIDAttr + "></td>");
-      $("#" + rowId).append(td);
+    for (let j = 1; j <= TOTAL_COLUMNS; j++) {
+      let colID = 'col' + j;
+      let colIDAttr = 'id="' + colID + '"';
+      let td = $('<td ' + colIDAttr + '></td>');
+      $('#' + rowId).append(td);
     }
   }
 }
